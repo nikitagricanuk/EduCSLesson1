c5d718d340312001253bce69c44db64c1ee78460
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-02-24 21:57:10 +0800

    Add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please enter first digit: ");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter second digit: ");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+if(digit1 > digit2) Console.WriteLine(Convert.ToString(digit1) + " is bigger");
+else Console.WriteLine(Convert.ToString(digit2) + " is bigger");
