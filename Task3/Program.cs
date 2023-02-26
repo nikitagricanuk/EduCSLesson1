2cd308fc5d2017e962b42b18fa2fa9decc5d1b5a
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-02-26 10:07:10 +0800

    Edit Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,4 +1,5 @@
-﻿int digit = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Enter digit: ");
+int digit = Convert.ToInt32(Console.ReadLine());
 
 if(digit % 2 == 0) Console.WriteLine(Convert.ToString(digit) + " -> Yes");
 else Console.WriteLine(Convert.ToString(digit) + " -> No");
