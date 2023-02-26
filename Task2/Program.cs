a3daa8eb939e40dec007fc571c7ef4713a427167
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-02-26 09:52:13 +0800

    Add Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,8 +5,6 @@ int digit2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter third digit: ");
 int digit3 = Convert.ToInt32(Console.ReadLine());
 
-int max = 0;
-
 if(digit1 > digit2 && digit1 > digit3) Console.Write(Convert.ToString(digit1) + " is bigger");
 else if(digit2 > digit3 && digit2 > digit1) Console.Write(Convert.ToString(digit2) + " is bigger");
 else Console.Write(Convert.ToString(digit3) + " is bigger");
